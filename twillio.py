9f03f49f99243923c802b10831b9e585eea2c41a
Fache Aurélien <aurelien.fache@gmail.com>
2022-04-05 08:59:31 +0200

    Update and rename twillio to twillio.py

--- twillio.py
+++ twillio.py
@@ -2,8 +2,8 @@ from twilio.rest import Client
 
 # Your Account Sid and Auth Token from twilio.com/console
 # DANGER! This is insecure. See http://twil.io/secure
-account_sid = 'AC190ca6ae58310dfbd200716e774424ab'
-auth_token = 'f427893497574698a3747ad2bc20252e'
+account_sid = 'XXX'
+auth_token = 'XXXX'
 client = Client(account_sid, auth_token)
 
 
